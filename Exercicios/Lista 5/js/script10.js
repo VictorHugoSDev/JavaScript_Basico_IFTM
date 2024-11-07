34b3e77179c2968465fc5651234fe76c66f26e5b
Victor Hugo Silva <90207063+VictorHugoSDev@users.noreply.github.com>
2024-11-07 12:49:54 -0300

    Update script10.js

--- Exercicios/Lista 5/js/script10.js
+++ Exercicios/Lista 5/js/script10.js
@@ -4,6 +4,15 @@ while(numero < 2 || !Number.isInteger(numero)){
     numero = parseInt(prompt("Valor incorreto, digite valor inteiro positivo maior ou igual a 2:"));
 }
 
-console.log("Número em decimal: " + numero);
-console.log("Número em binário: " + numero.toString(2));
-console.log("Número em hexadecimal: " + numero.toString(16).toUpperCase());
+document.write('<table border="1" cellpadding="10" cellspacing="0">');
+    document.write('<tr><th>Base Decimal</th><th>Base Binária</th><th>Base Hexadecimal</th></tr>');
+
+    for (let i = 0; i <= numero; i++) {
+        document.write('<tr>');
+            document.write('<td>' + i + '</td>');
+            document.write('<td>' + i.toString(2) + '</td>');
+            document.write('<td>' + i.toString(16).toUpperCase() + '</td>');
+        document.write('</tr>');
+    }
+
+ document.write('</table>');
