a47ddde6c41f7d685de29abb79f22bbd8da1c367
Victor Hugo Silva <90207063+VictorHugoSDev@users.noreply.github.com>
2024-11-08 16:05:58 -0300

    Add files via upload

---
+++ Exercicios/Lista 6/js/script1.js
@@ -0,0 +1,44 @@
+var boy = document.getElementById("boy");
+var textBoy = document.getElementById("textBoy");
+
+var personagem = {
+    alegre: { img: "img/alegre.png", msg: ", seja bem-vindo!" },
+    assustado: { img: "img/assustado.png", msg: "O que você quer?" },
+    nervoso: { img: "img/nervoso.png", msg: "Não me faça perder o meu tempo!!!" },
+    pensativo: { img: "img/pensativo.png", msg: "zzzzzzz!" }
+};
+
+function changeBoy(personagem) {
+    boy.src = personagem.img;
+    textBoy.innerHTML = personagem.msg;
+    
+    if (personagem === personagem.alegre) {
+        boy.classList.add('animateMove');
+        boy.classList.remove('shake');
+    } 
+    else if (personagem === personagem.nervoso) {
+        boy.classList.add('shake');
+        boy.classList.remove('animateMove');
+    } 
+    else {
+        boy.classList.remove('animateMove', 'shake');
+    }
+}
+
+boy.addEventListener("mouseenter", function () {
+    changeBoy(personagem.assustado);
+});
+
+boy.addEventListener("mouseout", function () {
+    changeBoy(personagem.pensativo);
+});
+
+boy.addEventListener("click", function () {
+    var nome = prompt("Qual é o seu nome?");
+    if (!nome)
+        changeBoy(personagem.nervoso);
+    else {
+        personagem.alegre.msg = nome + personagem.alegre.msg;
+        changeBoy(personagem.alegre);
+    }
+});
