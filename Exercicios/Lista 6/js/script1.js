a40afa5f2b286ad6c57c44cb2d01342457609cd0
Victor Hugo Silva <90207063+VictorHugoSDev@users.noreply.github.com>
2024-11-11 21:53:59 -0300

    Update script1.js

--- Exercicios/Lista 6/js/script1.js
+++ Exercicios/Lista 6/js/script1.js
@@ -2,21 +2,21 @@ var boy = document.getElementById("boy");
 var textBoy = document.getElementById("textBoy");
 
 var personagem = {
-    alegre: { img: "img/alegre.png", msg: ", seja bem-vindo!" },
-    assustado: { img: "img/assustado.png", msg: "O que você quer?" },
-    nervoso: { img: "img/nervoso.png", msg: "Não me faça perder o meu tempo!!!" },
-    pensativo: { img: "img/pensativo.png", msg: "zzzzzzz!" }
+    alegre: { img: "img/alegre.png", msg: ", seja bem-vindo!" , tipo: "alegre"},
+    assustado: { img: "img/assustado.png", msg: "O que você quer?" , tipo: "assustado"},
+    nervoso: { img: "img/nervoso.png", msg: "Não me faça perder o meu tempo!!!", tipo: "nervoso"},
+    pensativo: { img: "img/pensativo.png", msg: "zzzzzzz!", tipo: "pensativo"}
 };
 
 function changeBoy(personagem) {
     boy.src = personagem.img;
     textBoy.innerHTML = personagem.msg;
     
-    if (personagem === personagem.alegre) {
+    if (personagem.tipo === "alegre") {
         boy.classList.add('animateMove');
         boy.classList.remove('shake');
     } 
-    else if (personagem === personagem.nervoso) {
+    else if (personagem.tipo === "nervoso") {
         boy.classList.add('shake');
         boy.classList.remove('animateMove');
     } 
